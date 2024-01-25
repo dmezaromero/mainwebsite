cee601092b5e17a2b17af9827d27f3b76acdf61e
Dagoberto Meza <38015756+dmezaromero@users.noreply.github.com>
2024-01-25 18:54:23 +0000

    Add Education component to App.jsx and Header.jsx

---
+++ src/Components/Education.jsx
@@ -0,0 +1,22 @@
+
+import React from "react";
+
+const Education = () => {
+    return (
+        <section className="padding" id="education">
+        <h2 style={{ textAlign: "center" }}>Education</h2>
+        <div className="container">
+            <div className="box">
+            <h3>University of California, Irvine</h3>
+            <p className="small">Full Stack Web Development Certificate</p>
+            </div>
+            <div className="box">
+            <h3>University of California, Irvine</h3>
+            <p className="small">Bachelor of Arts in Business Economics</p>
+            </div>
+        </div>
+        </section>
+    );
+}
+
+export default Education;
